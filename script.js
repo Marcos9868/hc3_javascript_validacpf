06ebff789e61eed4f1a29ccf537b2dad6d8fc6f0
Marcos9868 <marcos_m_ferreira@outlook.com>
2022-05-11 10:31:42 -0300

    Adiciona verificação dos últimos dígitos do CPF

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 console.log('Javascript carregado')
 
 function validaCPF (cpf) {
-  return true
+  console.log(cpf.length)
+  if (cpf.length != 11) {
+    return false
+  } else {
+    let numeros = cpf.substring(0, 9)
+    let digitos = cpf.substring(9)
+
+    let soma = 0
+    for (let i = 10; i > 1; i--) {
+      soma += numeros.charAt(10 - i) * i
+    }
+    console.log(soma)
+
+    const resultadoValidacao = soma % 11 < 2 ? 0 : 11 - (soma % 11)
+
+    if (resultadoValidacao != digitos.charAt(0)) {
+      return false
+    }
+
+    console.log(digitos.toString().charAt(0))
+    return true
+  } 
 }
 
 function validacao() {
   console.log('Validando CPF')
   const cpfDigitado = document.getElementById('input_cpf').value
-  console.log(cpfDigitado)
-
   const resultado = validaCPF(cpfDigitado)
 
   if (resultado) {
